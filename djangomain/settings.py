d0ac2032dd9dd3ba40c146475e53d8b1b8c61369
Dima <solodow.mitya@yandex.by>
2022-05-05 10:12:36 +0300

    finish-web-site

--- djangomain/settings.py
+++ djangomain/settings.py
@@ -123,7 +123,6 @@ else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
