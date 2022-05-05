de9b70012118a10c1a373d32714102ef2526e0f2
Dima <solodow.mitya@yandex.by>
2022-05-05 09:54:48 +0300

    finish-web-site

--- djangomain/settings.py
+++ djangomain/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/img/'
+MEDIA_URL = '/media/'
 if DEBUG:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
@@ -122,7 +122,7 @@ if DEBUG:
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = [os.path.join(BASE_DIR, 'img')]
+MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- djangomain/urls.py
+++ djangomain/urls.py
@@ -6,12 +6,14 @@ from django.views.static import serve
 from djangomain import settings
 
 urlpatterns = [
+    url(r'^media/(?P<path>.*)$', serve, {'document_root':
+                                             settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root':
+                                              settings.STATIC_ROOT}),
+
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    ]
+]
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
-
-
-
